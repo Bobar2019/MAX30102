844f8d3e09729efe6fa7c94271c9c85448959437
Didier Dero <didierdero@MACM1.local>
2025-02-05 02:30:21 +0100

    Correction de DEVICE_CLASS_HEART_RATE dans sensor.py

--- components/max30102/sensor.py
+++ components/max30102/sensor.py
@@ -3,9 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import (
     UNIT_BEATS_PER_MINUTE,
-    UNIT_PERCENT,
-    DEVICE_CLASS_HEART_RATE,
-    DEVICE_CLASS_OXYGEN_SATURATION,
+    UNIT_PERCENT
 )
 
 DEPENDENCIES = ["i2c"]
@@ -13,11 +11,12 @@ DEPENDENCIES = ["i2c"]
 max30102_ns = cg.esphome_ns.namespace("max30102")
 MAX30102Sensor = max30102_ns.class_("MAX30102Sensor", cg.PollingComponent, sensor.Sensor)
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(MAX30102Sensor),
-    }
+CONFIG_SCHEMA = sensor.sensor_schema(
+    MAX30102Sensor,
+    accuracy_decimals=0,
+    unit_of_measurement=UNIT_BEATS_PER_MINUTE,
 ).extend(cv.polling_component_schema("1s"))
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[cv.GenerateID()])
